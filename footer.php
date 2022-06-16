f41437561b3e69f2fa9f73db7a8c80be2e6cabf4
Hieuhuynh <conghieuhuynh91@gmail.com>
2022-06-16 14:06:37 +0700

    footer styling

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 	</div><!-- Row End -->
-</div><!-- Container End -->
+	</div><!-- Container End -->
 
-<!--
+	<!--
 <div class="full-width footer-widget">
 	<div class="row">
 		<?php dynamic_sidebar("Footer"); ?>
@@ -10,31 +10,34 @@
 -->
 
 
-<footer>
-	<?php echo do_shortcode('[INSERT_ELEMENTOR id="85"]'); ?>
-</footer>
+	<footer>
+		<div class="css-vector-footer-top"></div>
+		<div class="css-vector-footer-bottom"></div>
+		<?php echo do_shortcode('[INSERT_ELEMENTOR id="85"]'); ?>
+	</footer>
 
-<!--
+	<!--
 <footer class="full-width" role="contentinfo">
 	<div class="row love-reverie">
 		<div class="large-12 columns">
-			<p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. <?php _e('Made with Love in','reverie'); ?> <a href="http://themefortress.com/reverie/" rel="nofollow" title="Reverie Framework">Reverie</a>.</p>
+			<p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. <?php _e('Made with Love in', 'reverie'); ?> <a href="http://themefortress.com/reverie/" rel="nofollow" title="Reverie Framework">Reverie</a>.</p>
 		</div>
 	</div>
 </footer>
 -->
 
-<?php wp_footer(); ?>
+	<?php wp_footer(); ?>
 
-<script>
-	(function($) {
-		$(document).foundation();
-	})(jQuery);
-</script>
+	<script>
+		(function($) {
+			$(document).foundation();
+		})(jQuery);
+	</script>
 	<style>
-img#loder_img {
-    display: none;
-}
-</style>
-</body>
-</html>
+		img#loder_img {
+			display: none;
+		}
+	</style>
+	</body>
+
+	</html>
