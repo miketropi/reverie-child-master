06e89c07273ae6ebda5e9bf0204f5e9cd44b65be
Hieuhuynh <tomdevvn90@gmail.com>
2022-06-29 08:36:10 +0700

    update category product page

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -79,17 +79,17 @@ do_action('woocommerce_before_main_content');
 					);
 
 					//$product_categories = get_terms('product_cat', $cat_args);
-					$product_categories = get_field('prod_cat_mobile', 'option');
+					$product_categories_menu = get_field('prod_cat_mobile', 'option');
 
 
-					if (!empty($product_categories)) {
+					if (!empty($product_categories_menu)) {
 					?>
 						<div class="e-product-cate-menu__inner">
 							<h4 class="hfp-widget__title">Categories</h4>
 							<ul class="p-term-list">
 								<?php $classes_all = 'all-products' == $term->slug ? 'active-item' : ''; ?>
-								<?php if ($product_categories && count($product_categories) > 0) {
-									foreach ($product_categories as $key => $category) {
+								<?php if ($product_categories_menu && count($product_categories_menu) > 0) {
+									foreach ($product_categories_menu as $key => $category) {
 										$classes = $category->slug == $term->slug ? 'active-item' : '';
 								?>
 										<li class="p-term-list__item <?= $classes ?>">
@@ -113,65 +113,74 @@ do_action('woocommerce_before_main_content');
 					?>
 				</div>
 				<div class="csscols cols-md-9">
-					<?php
-					echo '<h1 class="title-cat-heading">' . $term->name . '</h1>';
+					<?php					
 					echo '<div class="e-products-grid woocommerce">';
-					echo '<div class="e-products-grid__inner">';
-					$args = array(
-						'posts_per_page' => -1,
-						'tax_query' => array(
-							'relation' => 'AND',
-							array(
-								'taxonomy' => 'product_cat',
-								'field' => 'slug',
-								'terms' => $term->slug
-							),
-						),
-						'post_type' => 'product',
-						'orderby' => 'title',
-						'order' => 'ASC',
-					);
-					$cat_query = new WP_Query($args);
+					$i = 1;
+					foreach ($product_categories as $product_category) {
+						if ($product_category->parent != 0) {
+							echo '<h2 class="title-cat-heading">' . $product_category->name . '</h2>';
+							echo '<div class="e-products-grid__inner">';
+							$args = array(
+								'posts_per_page' => -1,
+								'tax_query' => array(
+									'relation' => 'AND',
+									array(
+										'taxonomy' => 'product_cat',
+										'field' => 'slug',
+										'terms' => $product_category->slug
+									),
+								),
+								'post_type' => 'product',
+								'orderby' => 'title',
+								'order' => 'ASC',
+							);
+							$cat_query = new WP_Query($args);
 
-					while ($cat_query->have_posts()) : $cat_query->the_post();
-						$product = wc_get_product($cat_query->post->ID);
-						$post_thumbnail_id = get_post_thumbnail_id();
-						$product_thumbnail = wp_get_attachment_image_src($post_thumbnail_id, $size = 'shop-feature');
-						$product_thumbnail_alt = get_post_meta($post_thumbnail_id, '_wp_attachment_image_alt', true);
-						$average = $product->get_average_rating();
-						$p_image = (($product_thumbnail && isset($product_thumbnail[0])) ? $product_thumbnail[0] : wc_placeholder_img_src());
+							while ($cat_query->have_posts()) : $cat_query->the_post();
+								$product = wc_get_product($cat_query->post->ID);
+								$post_thumbnail_id = get_post_thumbnail_id();
+								$product_thumbnail = wp_get_attachment_image_src($post_thumbnail_id, $size = 'shop-feature');
+								$product_thumbnail_alt = get_post_meta($post_thumbnail_id, '_wp_attachment_image_alt', true);
+								$average = $product->get_average_rating();
+								$p_image = (($product_thumbnail && isset($product_thumbnail[0])) ? $product_thumbnail[0] : wc_placeholder_img_src());
 					?>
-						<div class="e-products-grid__item">
-							<a class="thumb-prod" href="<?php the_permalink() ?>">
-								<img src="<?php echo $p_image; ?>" alt="<?php echo $product_thumbnail_alt; ?>">
-							</a>
-							<h2 class="woocommerce-loop-product__title">
-								<a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
-							</h2>
-							<div class="prod-star-rating">
-								<span class="prod-star prod-star-df"></span>
-								<span class="prod-star prod-star-active" style="width:<?= (($average / 5) * 100) ?>%"><span></span></span>
-							</div>
-							<?php if (!$product->get_short_description()) return; ?>
-							<div itemprop="description" class="product-description">
-								<?php echo apply_filters('woocommerce_short_description', $product->get_short_description()) ?>
-							</div>
-							<?php
-							if ($product->is_type('variable')) {
-								$product_variations = $product->get_available_variations();
-								$variation_product_id = $product_variations[0]['variation_id'];
-								$variation_product = new WC_Product_Variation($variation_product_id);
-							?>
-								<span class="price"><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">$</span><?php echo $variation_product->regular_price; ?></span></span></a>
-							<?php } else { ?>
-								<span class="price"><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">$</span><?php echo $product->get_regular_price(); ?></span></span></a>
-							<?php } ?>
-							<a href="<?php the_permalink(); ?>" class="btn-product-detail">View Product</a>
-						</div>
-					<?php
-					endwhile; // end of the loop.
-					wp_reset_postdata();
-					echo '</div>';
+								<div class="e-products-grid__item">
+									<a class="thumb-prod" href="<?php the_permalink() ?>">
+										<img src="<?php echo $p_image; ?>" alt="<?php echo $product_thumbnail_alt; ?>">
+									</a>
+									<h2 class="woocommerce-loop-product__title">
+										<a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
+									</h2>
+									<div class="prod-star-rating">
+										<span class="prod-star prod-star-df"></span>
+										<span class="prod-star prod-star-active" style="width:<?= (($average / 5) * 100) ?>%"><span></span></span>
+									</div>
+									<?php if (!$product->get_short_description()) return; ?>
+									<div itemprop="description" class="product-description">
+										<?php echo apply_filters('woocommerce_short_description', $product->get_short_description()) ?>
+									</div>
+									<?php
+									if ($product->is_type('variable')) {
+										$product_variations = $product->get_available_variations();
+										$variation_product_id = $product_variations[0]['variation_id'];
+										$variation_product = new WC_Product_Variation($variation_product_id);
+									?>
+										<span class="price"><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">$</span><?php echo $variation_product->regular_price; ?></span></span></a>
+									<?php } else { ?>
+										<span class="price"><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">$</span><?php echo $product->get_regular_price(); ?></span></span></a>
+									<?php } ?>
+									<a href="<?php the_permalink(); ?>" class="btn-product-detail">View Product</a>
+								</div>
+						<?php
+
+							endwhile; // end of the loop.
+							wp_reset_postdata();
+							echo '</div>';
+						}
+						if ($i < count($product_categories))
+							echo '<div class="content-seperator"></div>';
+						$i++;
+					} //foreach
 					echo '</div>';
 					?>
 				</div>
